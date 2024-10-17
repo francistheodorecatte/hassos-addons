71cbd46c12171fded99db9877ba8fae9dd83aabb
francistheodorecatte <perturbed.old.cat@gmail.com>
2024-10-17 13:13:48 -0400

    Update rcs_mon.sh to grab variables from rcs_req, assemble into json payload, and send down a fifo in /tmp.
    This will make asynchronously grabbing this payload via x10mqtt.py much, much simpler (no multi-line parsing of heyu monitor).

--- x10mqtt/data/rcs_mon.sh
+++ x10mqtt/data/rcs_mon.sh
@@ -1,17 +1,33 @@
+#!/bin/bash
 # quick script to build our own full thermostat auto report, when RCS failed to!
 
 # use the provided housecode variable from heyu to build the houseunit code
 hu="${X10_Housecode}5"
 
-# heyu should already check for a unit code of 6, so we just need to check if the preset level is 9.
-# we should have the current temperature (preset 1) due to an auto report, so call the rest ourselves.
-# use rcs_req to have heyu wait for the thermostat to respond, to avoid collisions.
-if [[ $X10_PresetLevel -eq 9 ]]; then
-        heyu rcs_req preset $hu 2
-        heyu rcs_req preset $hu 3
-        heyu rcs_req preset $hu 4
-        heyu rcs_req preset $hu 5
-        heyu rcs_req preset $hu 6
+pipe="/tmp/rcsmon"
+
+# heyu should already check for a unit code of 6, so we just need to check if the preset level is 9 or 10.
+# we don't care what auto send status register called this script, so just request status from all six registers.
+# use rcs_req to request each register directly w/o having to parse live heyu monitor, then mangle output into usability.
+# then concat all register variables into a json payload for x10mqtty, and send down a named pipe placed in /tmp.
+if [[ $X10_PresetLevel -eq 9 ]] || [[ $X10_PresetLEvel -eq 10 ]]; then
+
+        # check if our pipe exists; create it if not.
+        if [[ ! -p $pipe ]]; then
+                mkfifo $pipe
+        fi
+
+        temp=`heyu rcs_req preset ${hu} 1 | cut -d "=" -f2- | cut -d ":" -f1 | awk '{gsub(/^ +| +$/,"")} {print $0}'`
+        setpnt=`heyu rcs_req preset ${hu} 2 | cut -d "=" -f2- | cut -d ":" -f1 | awk '{gsub(/^ +| +$/,"")} {print $0}'`
+        mode=`heyu rcs_req preset ${hu} 3 | cut -d "=" -f2- | cut -d ":" -f1 | awk '{gsub(/^ +| +$/,"")} {print $0}'`
+        fanstat=`heyu rcs_req preset ${hu} 4 | cut -d "=" -f2- | cut -d ":" -f1 | awk '{gsub(/^ +| +$/,"")} {print $0}'`
+        sbmode=`heyu rcs_req preset ${hu} 5 | cut -d "=" -f2- | cut -d ":" -f1 | awk '{gsub(/^ +| +$/,"")} {print $0}'`
+        sbdelt=`heyu rcs_req preset ${hu} 6 | cut -d "=" -f2- | cut -d ":" -f1 | awk '{gsub(/^ +| +$/,"")} {print $0}'`
+
+        # assemble the payload and echo it down the pipe (will wait in background until x10mqtt reads it in)
+        payload="{\"temperature\": \"$temp\", \"setpoint\": \"$setpnt\", \"mode\": \"$mode\", \"fan\": \"$fanstat\", \"sb_mode\": \"$sbmode\", \"sb_delta\": \"$sbdelt\"}"
+        echo $payload > $pipe &
+
 fi
 
 exit
